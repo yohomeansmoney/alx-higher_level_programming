54becb5eecd5c542e1296f00ac0a1b2b27e69daf
daniel nyoho <danielnyoho@gmail.com>
2023-04-04 12:31:05 -0700

    Everything is object

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def magic_string(listring=[]):
+    listring += ["BestSchool"]
+    return (", ". join(listring))
---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Locked class module"""
+
+
+class LockedClass:
+    """class locked for class"""
+    __slots__ = ('first_name')
